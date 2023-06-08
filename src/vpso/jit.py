af5f433bdbc5ca182d28d7af1e73518af37661f7
Filippo Airaldi <f.airaldi@tudelft.nl>
2023-06-08 21:53:35 +0200

    simplified jit naming

--- src/vpso/jit.py
+++ src/vpso/jit.py
@@ -4,55 +4,57 @@ import numba as nb
 
 _float = nb.float64
 _int = nb.int32
+_array2d = _float[:, :]
+_array3d = _float[:, :, :]
 
 _signatures = {
-    "_pso_equation": nb.types.UniTuple(_float[:, :, :], 2)(
-        _float[:, :, :],  # x
-        _float[:, :, :],  # px
-        _float[:, :],  # sx
-        _float[:, :, :],  # v
-        _float[:, :, :],  # v_max
+    "_pso_equation": nb.types.UniTuple(_array3d, 2)(
+        _array3d,  # x
+        _array3d,  # px
+        _array2d,  # sx
+        _array3d,  # v
+        _array3d,  # v_max
         _float,  # w
         _float,  # c1
         _float,  # c2
         nb.types.NumPyRandomGeneratorType("NumPyRandomGeneratorType"),
     ),
-    "_repair_out_of_bounds": nb.types.UniTuple(_float[:, :, :], 2)(
-        _float[:, :, :],  # x
-        _float[:, :, :],  # x_new
-        _float[:, :, :],  # v_new
-        _float[:, :, :],  # px
-        _float[:, :],  # sx
-        _float[:, :, :],  # v
-        _float[:, :, :],  # v_max
-        _float[:, :, :],  # lb
-        _float[:, :, :],  # ub
+    "_repair_out_of_bounds": nb.types.UniTuple(_array3d, 2)(
+        _array3d,  # x
+        _array3d,  # x_new
+        _array3d,  # v_new
+        _array3d,  # px
+        _array2d,  # sx
+        _array3d,  # v
+        _array3d,  # v_max
+        _array3d,  # lb
+        _array3d,  # ub
         _float,  # w
         _float,  # c1
         _float,  # c2
         _int,  # iters
         nb.types.NumPyRandomGeneratorType("NumPyRandomGeneratorType"),
     ),
-    "_polynomial_mutation": _float[:, :, :](
-        _float[:, :, :],  # x
-        _float[:, :, :],  # px
-        _float[:, :],  # pf
-        _float[:, :, :],  # lb
-        _float[:, :, :],  # ub
+    "_polynomial_mutation": _array3d(
+        _array3d,  # x
+        _array3d,  # px
+        _array2d,  # pf
+        _array3d,  # lb
+        _array3d,  # ub
         _int,  # nvec
         _int,  # dim
         _float,  # mutation_prob
         nb.types.NumPyRandomGeneratorType("NumPyRandomGeneratorType"),
     ),
-    "_generate_offsprings": nb.types.UniTuple(_float[:, :, :], 2)(
-        _float[:, :, :],  # x
-        _float[:, :, :],  # px
-        _float[:, :],  # pf
-        _float[:, :],  # sx
-        _float[:, :, :],  # v
-        _float[:, :, :],  # v_max
-        _float[:, :, :],  # lb
-        _float[:, :, :],  # ub
+    "_generate_offsprings": nb.types.UniTuple(_array3d, 2)(
+        _array3d,  # x
+        _array3d,  # px
+        _array2d,  # pf
+        _array2d,  # sx
+        _array3d,  # v
+        _array3d,  # v_max
+        _array3d,  # lb
+        _array3d,  # ub
         _int,  # nvec
         _int,  # dim
         _float,  # w
