3ebebff413d397e36b798e93a19d723a4fe205b5
Filippo Airaldi <f.airaldi@tudelft.nl>
2023-06-11 18:50:11 +0200

    added typing for int arrays

--- src/vpso/typing.py
+++ src/vpso/typing.py
@@ -1,7 +1,9 @@
-from numpy import floating
+from numpy import double, int_
 from numpy.typing import NDArray
 from typing_extensions import TypeAlias
 
-Array1d: TypeAlias = NDArray[floating]
-Array2d: TypeAlias = NDArray[floating]
-Array3d: TypeAlias = NDArray[floating]
+Array1i: TypeAlias = NDArray[int_]
+Array3i: TypeAlias = NDArray[int_]
+Array1d: TypeAlias = NDArray[double]
+Array2d: TypeAlias = NDArray[double]
+Array3d: TypeAlias = NDArray[double]
