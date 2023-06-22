129038c61880b0610d8e603e6c07f34cd6958493
Filippo Airaldi <f.airaldi@tudelft.nl>
2023-06-23 01:05:56 +0200

    added numba config file

--- .cmd
+++ .cmd
@@ -1,7 +1,5 @@
 autoflake --remove-all-unused-imports --remove-unused-variables --remove-duplicate-keys --in-place --recursive .
 isort .
 black .
-
-set NUMBA_DISABLE_JIT=1
 coverage run -m unittest discover tests
 coverage xml
